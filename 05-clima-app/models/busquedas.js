aeab73d164f766708ea86848e5297286d5e5262f
Aaron Rincon <rlaaron0305@gmail.com>
2022-12-04 00:06:05 -0600

    update city

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -19,20 +19,29 @@ class Busquedas {
     async ciudad( lugar = '' ){
         try {
 
-            // const instance = axios.create({
-            //     baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
-            //     params: this.paramsMapbox
-            // });
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
 
-            // const resp = await instance.get();
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/puebla.json?limit=5&language=es&access_token=pk.eyJ1IjoiYWFyb25ybDAzIiwiYSI6ImNrenJtdzViMjE0eHgydm5hbWhzM3Btbm8ifQ.RnPMhR0InHaqOxVeJTYxWw');
-            console.log(resp.data);
+            const resp = await instance.get();
 
-            return [];
+            return resp.data.features.map( lugar => ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1]
+             }));
+
+            
+            
         } catch (error) {
             // return [];
-            console.log("errro");
+            console.log("errror");
+            console.log(error);
         }
     }
+
+    
 }
 module.exports = Busquedas;
